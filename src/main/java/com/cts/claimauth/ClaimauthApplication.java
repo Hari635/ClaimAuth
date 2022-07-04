c7a2df728be5253e5786bccdc2d6cb1bbb764e02
Harihara Suthan <hariharasuthan81@gmail.com>
2022-07-04 09:58:12 +0530

    Initial commit

---
+++ src/main/java/com/cts/claimauth/ClaimauthApplication.java
@@ -0,0 +1,13 @@
+package com.cts.claimauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cts/claimauth/ClaimauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.claimauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
